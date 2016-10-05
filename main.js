871d40e785b9d9e3bb5d0e48a33df4e919a4e12e
jkillz2020 <kilgore.josh@gmail.com>
2016-10-04 20:54:42 -0500

    dog food loading to DOM

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-var dogFoodString = "";
+var dogData = [];
 var currentDogFood; 
+displayDiv ="";
 var contentEl = document.getElementById("dog");
 
 
-  // console.log(dogFoodData);
-  contentEl.innerHTML = dogFoodString;
-
 var myRequest = new XMLHttpRequest();
-// console.log("myRequest", myRequest);
 
 myRequest.addEventListener("load", loadDogFood); //Callback
 //myRequest.addEventListener("error")
@@ -15,20 +12,32 @@ myRequest.addEventListener("load", loadDogFood); //Callback
 myRequest.open("GET", "dogfood.json")
 myRequest.send();
 
-// myRequest.addEventListener("load", executeThisCodeAfterFileLoaded); //Callback
-// myRequest.addEventListener("error", executeThisCodeIfXHRFails)
-// myRequest.addEventListener("progress", executeThisCodeWhenChunksArrive)
-// myRequest.open("GET", "https://teamtreehouse.com/zoeleblanc.json")
-// myRequest.send();
-
 function loadDogFood() {
-var dogFoodData = JSON.parse(this.responseText);
-console.log(dogFoodData);
-};
-// for (var i = 0; i < dogFoodData.name.length; i++) {
-//     currentDogFood = dogFoodData.name[i];
+var dogData = JSON.parse(this.responseText).dog_brands;
 
-  //   dogFoodString += "<div>";
-  //     dogFoodString += `<h1>${currentDogFood.name}</h1>`;
-  //   dogFoodString += "</div>";
-  // };
+for (var i = 0; i < dogData.length; i++) {
+    console.log(dogData);
+     dogFoodLoop = dogData[i];
+
+  displayDiv +=
+    // dogFoodData = dogData[i].name;
+    `<div>
+    <h1>${dogFoodLoop.name}</h1>
+    <hr>
+    <h2>${dogFoodLoop.types[0].type}</h2>
+    <h3>${dogFoodLoop.types[0].volumes[0].name}</h3>
+    <h3>${dogFoodLoop.types[0].volumes[0].price}</h3>
+    <h3>${dogFoodLoop.types[0].volumes[1].name}</h3>
+    <h3>${dogFoodLoop.types[0].volumes[1].price}</h3>
+    <h2>${dogFoodLoop.types[1].type}</h2>
+    <h3>${dogFoodLoop.types[1].volumes[0].name}</h3>
+    <h3>${dogFoodLoop.types[1].volumes[0].price}</h3>
+    <h3>${dogFoodLoop.types[1].volumes[1].name}</h3>
+    <h3>${dogFoodLoop.types[1].volumes[1].price}</h3>
+    </div>
+    `
+  contentEl.innerHTML = displayDiv;
+  };
+};
+
+//listDogFood();
